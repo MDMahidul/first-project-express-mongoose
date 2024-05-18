cb843a10f7b7db58f8ff12430dd162d189d1b3d9
MDMahidul <mmbmahidul007@gmail.com>
2024-05-18 16:33:11 +0600

    created model and schema

---
+++ src/app/modules/student.model.ts
@@ -0,0 +1,51 @@
+import { Schema, model, connect } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student/student.interface';
+
+// 2. Create a Schema corresponding to the document interface.
+//sub-schema-1
+const userNameSchema = new Schema<UserName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String },
+  lastName: { type: String, required: true },
+});
+//sub-schema-2
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContactNo: { type: String, required: true },
+});
+//sub-schema-3
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+//main schema
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: ['male', 'female'],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'],
+  presentAddress: { type: String, required: true },
+  parmanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImage: { type: String },
+  isActive: ['active', 'blocked'],
+});
+
+// 3. Create a Model.
+const User = model<Student>('Student',studentSchema)
