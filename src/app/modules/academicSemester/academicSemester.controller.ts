9016e8840ae290a12b546f53eb4ee80c748665b6
MDMahidul <mmbmahidul007@gmail.com>
2024-05-30 01:37:27 +0600

    get all and single semester data and update single semester data

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,20 +1,59 @@
-import httpStatus from "http-status";
-import sendResponse from "../../utils/sendResponse";
-import { AcademicSemesterServices } from "./academicSemester.service";
-import catchAsync from "../../utils/catchAsync";
-import { RequestHandler } from "express";
+import httpStatus from 'http-status';
+import sendResponse from '../../utils/sendResponse';
+import { AcademicSemesterServices } from './academicSemester.service';
+import catchAsync from '../../utils/catchAsync';
+import { RequestHandler } from 'express';
 
-const createAcademicSemester: RequestHandler = catchAsync(
+const createAcademicSemester: RequestHandler = catchAsync(async (req, res) => {
+  const result = await AcademicSemesterServices.createAcademicSemesterIntoDB(
+    req.body,
+  );
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Academic semseter is created successfully!',
+    data: result,
+  });
+});
+
+const getAllAcademicSemester: RequestHandler = catchAsync(async (req, res) => {
+  const result = await AcademicSemesterServices.getAllAcademicSemester();
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'All Academic semseters is retrived successfully!',
+    data: result,
+  });
+});
+const getSingleAcademicSemester: RequestHandler = catchAsync(
   async (req, res) => {
-
-    const result = await AcademicSemesterServices.createAcademicSemesterIntoDB(
-      req.body
-    );
+    const { semesterId } = req.params;
+    console.log(semesterId);
+    const result =
+      await AcademicSemesterServices.getSingleAcademicSemester(semesterId);
 
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
-      message: 'Academic semseter is created successfully',
+      message: 'Academic semseter is retrived successfully!',
+      data: result,
+    });
+  },
+);
+
+const updateSingleAcademicSemester: RequestHandler = catchAsync(
+  async (req, res) => {
+    const { semesterId } = req.params;
+    const semesterUpdatedData = req.body;
+    const result =
+      await AcademicSemesterServices.updateSingleAcademicSemester(semesterId,semesterUpdatedData);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: 'Academic semseter updated successfully!',
       data: result,
     });
   },
@@ -22,4 +61,7 @@ const createAcademicSemester: RequestHandler = catchAsync(
 
 export const AcademicSemesterControllers = {
   createAcademicSemester,
+  getAllAcademicSemester,
+  getSingleAcademicSemester,
+  updateSingleAcademicSemester,
 };
--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -5,6 +5,12 @@ import { AcademicSemesterValidations } from './academicSemester.validation';
 
 const router = express.Router();
 
-router.post('/create-academic-semester',validateRequest(AcademicSemesterValidations.createAcademicSchemaValidation),AcademicSemesterControllers.createAcademicSemester)
+router.post('/create-academic-semester',validateRequest(AcademicSemesterValidations.createAcademicSchemaValidation),AcademicSemesterControllers.createAcademicSemester);
+
+router.get('/',AcademicSemesterControllers.getAllAcademicSemester);
+
+router.get('/:semesterId',AcademicSemesterControllers.getSingleAcademicSemester);
+
+router.patch('/:semesterId',validateRequest(AcademicSemesterValidations.updateAcademicSchemaValidation),AcademicSemesterControllers.updateSingleAcademicSemester);
 
 export const AcademicSemesterRouters=router;
