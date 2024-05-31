0b02abf0f5107fe693cfff8c96dfdfc51d24e0cd
MDMahidul <mmbmahidul007@gmail.com>
2024-05-31 16:40:03 +0600

    created academic faculty schema,model and insert and get and update data into db

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,8 @@
-import { Router } from "express";
-import { StudentRoutes } from "../modules/student/student.route";
-import { UserRoutes } from "../modules/user/user.routes";
-import { AcademicSemesterRouters as AcademicSemesterRoutes } from "../modules/academicSemester/academicSemester.routes";
+import { Router } from 'express';
+import { StudentRoutes } from '../modules/student/student.route';
+import { UserRoutes } from '../modules/user/user.routes';
+import { AcademicSemesterRouters as AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
+import { AcademicFacultyRouters } from '../modules/academicFaculty/academicFaculty.routes';
 
 const router = Router();
 
@@ -18,8 +19,12 @@ const moduleRoutes = [
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
   },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRouters,
+  },
 ];
 
-moduleRoutes.forEach(route=>router.use(route.path,route.route))
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
-export default router
+export default router;
