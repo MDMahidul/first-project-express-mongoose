139efacc3d0dec7ff2058c22b9519c63600891ac
MDMahidul <mmbmahidul007@gmail.com>
2024-06-05 13:22:05 +0600

    handle zod error and show error messgaes

---
+++ src/app/interface/error.ts
@@ -0,0 +1,6 @@
+// create an array of object
+export type TErrorSource = {
+  path: string | number;
+  message: string;
+}[];
+
--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -1,13 +1,34 @@
-import { Request, Response, NextFunction } from 'express';
+import {ErrorRequestHandler } from 'express';
+import { ZodError} from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
+import { handleZodError } from '../errors/handleZodError';
 
-const globalErrorHandler=(err: any, req: Request, res: Response, next: NextFunction) => {
-  const statusCode =err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // set default values
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+ 
+  let errorSources: TErrorSource = [
+    { path: '', message: 'Something went wrong!' },
+  ];
 
+
+
+  // check the error provider
+  if (err instanceof ZodError) {
+    const simplified = handleZodError(err);
+    statusCode=simplified?.statusCode;
+    message=simplified?.message;
+    errorSources=simplified?.errorSources;
+  }
+
+  // ultimate return
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack:config.NODE_ENV === 'development' ?  err?.stack : null
   });
 };
 
