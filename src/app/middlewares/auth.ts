24a4671cd96c33b8be98299db312ce479bbc125c
MDMahidul <mmbmahidul007@gmail.com>
2024-06-12 23:58:36 +0600

    implemented admin authorization for createing users

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -4,37 +4,44 @@ import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
+import { TUserRole } from '../modules/user/user.interface';
 
 /* middleware */
-const auth = () => {
-  return catchAsync(
-    async (req: Request, res: Response, next: NextFunction) => {
-      const token = req.headers.authorization;
+const auth = (...requiredRoles: TUserRole[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
 
-      // check if the token sent from client
-      if (!token) {
-        throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
-      }
+    // check if the token sent from client
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
 
-      // check if the token is valid
-      jwt.verify(
-        token,
-        config.jwt_access_secret as string,
-        function (err, decoded) {
-          if (err) {
-            throw new AppError(
-              httpStatus.UNAUTHORIZED,
-              'You are not authorized!',
-            );
-          }
+    // check if the token is valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        if (err) {
+          throw new AppError(
+            httpStatus.UNAUTHORIZED,
+            'You are not authorized!',
+          );
+        }
 
-          req.user = decoded as JwtPayload;
+        // check user role
+        const role = (decoded as JwtPayload).role;
+        if (requiredRoles && !requiredRoles.includes(role)) {
+          throw new AppError(
+            httpStatus.UNAUTHORIZED,
+            'You are not authorized!',
+          );
+        }
+
+        req.user = decoded as JwtPayload;
         next();
-        },
-      );
-
-    },
-  );
+      },
+    );
+  });
 };
 
 export default auth;
--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,4 +1,5 @@
 import { Model } from 'mongoose';
+import { USER_ROLE } from './user.constant';
 
 export interface TUser {
   id: string;
@@ -15,3 +16,6 @@ export interface UserModel extends Model<TUser> {
   
   isPasswordMatched(palinTextPassword:string, hashedPassword:string): Promise<boolean>;
 }
+
+
+export type TUserRole = keyof typeof USER_ROLE;
